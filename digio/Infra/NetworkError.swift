7048e21f41c56243792a4d53146cbbae5755c7ba
ianfagundes <ianfag@icloud.com>
2024-08-22 18:47:09 -0300

    CHORE: Adicionado o código aos arquivos HttpGetClient, NetworkError.
    - Renomeado HttpGetClient para HttpGetClientProtocol para uma melhor legibilidade.

---
+++ digio/Infra/NetworkError.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkError.swift
+//  digio
+//
+//  Created by Ian Fagundes on 22/08/24.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+    case httpError(Int)
+    case unknown
+}
